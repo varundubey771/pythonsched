be9ae43389f2311ca3b0c99e82ad0c9544ae1fb6
varundubey771 <49694579+varundubey771@users.noreply.github.com>
2023-08-27 19:36:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,27 @@ from flask import Flask, request
 import requests
 from bs4 import BeautifulSoup
 import re
+import asyncio
+from prisma import Prisma
+from prisma.models import JobCount
+import datetime
 
 
 # Load data from config.ini file
 # config = configparser.ConfigParser()
 # config.read('config.ini')
+
+async def writer(data):
+    db = Prisma(auto_register=True)
+    await db.connect()
+
+    # write your queries here
+    resp = await JobCount.prisma().create_many(
+        data=data
+    )
+    await db.disconnect()
+    return resp
+
 def scrape_jobs(country,jobTitle):
     url = f"https://www.linkedin.com/jobs/search/?keywords={jobTitle}&location={country}"
 
@@ -36,7 +52,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'welcome'
+   return 'Hello, what did you have for breakfast?'
 
 @app.route('/getJobData')
 def getJobData():
@@ -48,6 +64,21 @@ def getJobData():
            job_count = scrape_jobs(country,job)
            lst.append({"country": country,"job_count":job_count,"job":job})
    return lst
+
+@app.route('/sendJobDataCron')
+def sendJobDataCron():
+   countries = ["Australia","India","United Kingdom","United States","Ireland"]
+   jobs = ["Python", "Java", "Data Scientist"]
+   lst=[]
+   current_datetime = datetime.datetime.now()
+   str_cur_dt= current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+   parsed_datetime = datetime.datetime.strptime(str_cur_dt, "%Y-%m-%d %H:%M:%S")    
+   for country in countries:
+       for job in jobs:
+           job_count = int(scrape_jobs(country,job))
+           lst.append({"country": country, "count": job_count, "title": job, "timestamp": parsed_datetime})
+   resp = asyncio.run(writer(lst))        
+   return resp
 
 if __name__ == "__main__":
     # Running server

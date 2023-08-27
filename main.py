dc52991cad0a312999eff2380e1069a0b837dd09
varundubey771 <49694579+varundubey771@users.noreply.github.com>
2023-08-28 00:02:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ import asyncio
 from prisma import Prisma
 from prisma.models import JobCount
 import datetime
+import subprocess
+
+generate_command = ["npx", "prisma", "generate"]
+subprocess.run(generate_command, check=True)
+
+
+migrate_command = ["npx", "prisma", "migrate", "deploy"]
+subprocess.run(migrate_command, check=True)
 
 
 # Load data from config.ini file
